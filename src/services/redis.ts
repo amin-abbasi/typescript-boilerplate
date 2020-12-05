5eab3c4af8f42ceb2e53f1d08dfe5fb1c5e13c48
Amin Abbasi <amin4193@gmail.com>
2020-12-05 13:26:50 +0330

    move typed pnms to dev-dependency, fix redis issue

--- src/services/redis.ts
+++ src/services/redis.ts
@@ -1,5 +1,4 @@
 // -------------------------------------- Initialize redis + config --------------------------------------
-
 import redis  from 'redis'
 import config from '../configs/config'
 
@@ -17,32 +16,9 @@ const options: redis.ClientOpts = {
   // }
 }
 if(REDIS_PASS) options.password = REDIS_PASS
-// const client = redis.createClient(options)
+const client = redis.createClient(options)
 
-// import { promisify }  from 'util'
-// const getAsync  = promisify(client.get).bind(client)
-
-// client.on('connect', () => { console.log(`<<<< Connected to Redis >>>>`) })
-// client.on('error', err => { console.log(`Redis Error: ${err}`) })
-
-// Redis functions
-// function create(id: any, value: any, type: any) {
-//   return Promise.resolve( client.set(`${type}:${id}`, JSON.stringify(value)) )
-// }
-
-// async function fetch(keyPattern: string) {
-//   try {
-//     const result = await getAsync(keyPattern)
-//     console.log('>>>>>>>> getAsync result: ', result)
-//     if(!result) return false
-//     return JSON.parse(result)
-//   }
-//   catch (err) {
-//     console.log('Redis Error - Fetch Data: ', err)
-//     throw err
-//   }
-// }
-
-// const exportResult = { create, fetch }
+client.on('connect', () => { console.log(`<<<< Connected to Redis >>>>`) })
+client.on('error', err => { console.log(`Redis Error: ${err}`) })
 
 export default client
