4313a5a8b090db42ec3281975db3ef3b1cdadb6f
Amin Abbasi <amin4193@gmail.com>
2021-08-24 12:47:35 +0430

    change mongodb debug config

--- src/services/database/mongo.ts
+++ src/services/database/mongo.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 import config   from '../../configs'
 
 // Database URL
-const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS } = config.env
+const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS, NODE_ENV } = config.env
 const dbURL = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`
 
 // Import the mongoose module
@@ -23,7 +23,7 @@ if(DB_USER && DB_PASS) {
 async function connectDB(): Promise<mongoose.Connection> {
 
   // Mongoose Debug Mode [set it as `false` in production]
-  mongoose.set('debug', true)
+  mongoose.set('debug', (NODE_ENV === 'development'))
 
   mongoose.connect(dbURL, options)
   mongoose.Promise = global.Promise // Get Mongoose to use the global promise library
