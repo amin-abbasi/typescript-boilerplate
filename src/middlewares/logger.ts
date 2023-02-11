c9137dba3088fcb9160cdce0484c2e41d7c23315
Amin Abbasi <amin4193@gmail.com>
2023-02-11 11:07:16 +0300

    feat: modify logger service

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -1,14 +1,24 @@
 import { Request, Response, NextFunction } from 'express'
 import gach from 'gach'
+import { colors } from 'gach/config'
 
-const getProcessingTimeInMS = (time: [number, number]): string => {
+const processTimeInMS = (time: [number, number]): string => {
   return `${(time[0] * 1000 + time[1] / 1e6).toFixed(2)}ms`
+}
+
+const color = (text: string, color: colors): string => gach(text).color(color).text
+
+const statusColor = (status: number): string => {
+  if(status >= 200 && status < 300) return gach(status.toString()).color('lightGreen').text
+  if(status >= 300 && status < 400) return gach(status.toString()).color('lightRed').text
+  if(status >= 400 && status < 500) return gach(status.toString()).color('lightYellow').text
+  if(status >= 500) return gach(status.toString()).color('red').text
+  return gach(status.toString()).color('red').text
 }
 
 /**
  * Print logs for API endpoints using the following pattern:
  * `[timestamp] method: url response.statusCode processingTime`
- *
  * @param mode mode to show extra information in log `short` or `full`
  */
 export function logger(mode: 'short' | 'full' = 'short'): (req: Request, res: Response, next: NextFunction) => void {
@@ -21,14 +31,14 @@ export function logger(mode: 'short' | 'full' = 'short'): (req: Request, res: Re
 
       res.once('finish', () => {
         const end = process.hrtime(start)
-        const endText = gach(`${getProcessingTimeInMS(end)}`).color('green').text
-        const status = gach(res.statusCode.toString()).color('yellow').text
+        const endText = gach(`${processTimeInMS(end)}`).color('green').text
+        const status = statusColor(res.statusCode)
 
         if(mode === 'full') {
           const { headers, body, params, query } = req
           const request = JSON.stringify({ headers, params, query, body })
-          console.log(`${timeStampText} ${method}: ${url} ${request} ${status} ${endText}`)
-        } else console.log(`${timeStampText} ${method}: ${url} ${status} ${endText}`)
+          console.log(`${timeStampText} ${color(method, 'yellow')}: ${url} ${request} ${status} ${endText}`)
+        } else console.log(`${timeStampText} ${color(method, 'yellow')}: ${url} ${status} ${endText}`)
       })
 
       next()
