fdddd84fd7f59fff8d3422c166d3d97b07865772
Amin Abbasi <amin4193@gmail.com>
2024-01-21 14:07:01 +0300

    feat: modify jwt functions to Token class

--- src/middlewares/check_auth.ts
+++ src/middlewares/check_auth.ts
@@ -3,7 +3,7 @@ import Errors from '../services/http_errors'
 import config from '../configs'
 import { UserAuth } from '../configs/types'
 import { MESSAGES } from './i18n/types'
-import { isValid } from '../services/jwt'
+import Token from '../services/token'
 
 // Function to set needed header auth
 export async function checkToken(
@@ -17,7 +17,7 @@ export async function checkToken(
     )[1]
     if (!authToken || authToken === '')
       throw Errors.Unauthorized(MESSAGES.INVALID_ACCESS_TOKEN)
-    const user = await isValid(authToken)
+    const user = await Token.isValid(authToken)
     if (!user) throw Errors.Unauthorized(MESSAGES.INVALID_ACCESS_TOKEN)
     req.user = user as UserAuth
     next()
